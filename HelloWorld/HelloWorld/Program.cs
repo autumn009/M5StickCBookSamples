d3c4ce100a225410f8ecacfea1955f2e755aeaca
Akira Kawamata <autumn@piedey.co.jp>
2024-02-03 10:55:38 +0900

    more simple

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -2,20 +2,11 @@ using nanoFramework.M5Stack;
 using System.Diagnostics;
 using System.Threading;
 
-namespace HelloWorld
-{
-    public class Program
-    {
-        public static void Main()
-        {
-            Debug.WriteLine("HELLO WORLD to debug window");
+Debug.WriteLine("HELLO WORLD to debug window");
 
-            M5StickC.InitializeScreen();
-            Console.Clear();
-            Console.ForegroundColor = System.Drawing.Color.Green;
-            Console.WriteLine("HELLO WORLD to screen");
+M5StickC.InitializeScreen();
+Console.Clear();
+Console.ForegroundColor = System.Drawing.Color.Green;
+Console.WriteLine("HELLO WORLD to screen");
 
-            Thread.Sleep(Timeout.Infinite);
-        }
-    }
-}
+Thread.Sleep(Timeout.Infinite);
