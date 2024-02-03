0f169f59e357f26724935283464e755fa956a412
Akira Kawamata <autumn@piedey.co.jp>
2024-02-03 16:36:35 +0900

    temp save

--- MultiWebServer/MultiWebServer/Program.cs
+++ MultiWebServer/MultiWebServer/Program.cs
@@ -1,13 +1,17 @@
 using MultiWebServer;
 using nanoFramework.M5Stack;
 using nanoFramework.Networking;
+using nanoFramework.WebServer;
 using System;
+using System.Device.Gpio;
 using System.Diagnostics;
 using System.IO;
+using System.Net;
 using System.Reflection;
+using System.Text;
 using System.Threading;
 
-Debug.WriteLine("Starting...");
+Debug.WriteLine("Initializing...");
 
 string ssid = "";
 string password = "";
@@ -15,6 +19,9 @@ GetSsidAndPassword();
 
 bool success;
 CancellationTokenSource cs = new(60000);
+
+Debug.WriteLine($"ssid={ssid} password={password}");
+
 success = WifiNetworkHelper.ConnectDhcp(ssid, password, requiresDateTime: true, token: cs.Token);
 if (!success)
 {
@@ -25,17 +32,50 @@ if (!success)
     }
     return;
 }
+Debug.WriteLine("Starting...");
+using (WebServer server = new WebServer(80, HttpProtocol.Http, new Type[0]))
+{
+    // Add a handler for commands that are received by the server.
+    server.CommandReceived += ServerCommandReceived;
+    // Start the server.
+    server.Start();
 
+    Thread.Sleep(Timeout.Infinite);
+}
 
+void ServerCommandReceived(object source, WebServerEventArgs e)
+{
+    try
+    {
+        var url = e.Context.Request.RawUrl;
+        Debug.WriteLine($"Command received: {url}, Method: {e.Context.Request.HttpMethod}");
 
-Thread.Sleep(Timeout.Infinite);
+        if (url.ToLower() == "/sayhello")
+        {
+            // This is simple raw text returned
+            WebServer.OutPutStream(e.Context.Response, "It's working, url is empty, this is just raw text, /sayhello is just returning a raw text");
+        }
+        else if (url.Length <= 1)
+        {
+            // Here you can return a real html page for example
+
+            WebServer.OutPutStream(e.Context.Response, "<html><head>" +
+                "<title>Hi from nanoFramework Server</title></head>" +
+                "<body>This is a default page</body></html>");
+        }
+    }
+    catch (Exception)
+    {
+        WebServer.OutputHttpCode(e.Context.Response, HttpStatusCode.InternalServerError);
+    }
+}
 
 void GetSsidAndPassword()
 {
     var r = Resources.GetBytes(Resources.BinaryResources.netinfo);
     using (var sr = new StreamReader(new MemoryStream(r)))
     {
-        ssid = sr.ReadLine();
-        password = sr.ReadLine();
+        ssid = sr.ReadLine().Trim();
+        password = sr.ReadLine().Trim();
     }
 }
