198378b61e729d8eb9e369f067fbb253b70c1f88
Akira Kawamata <autumn@piedey.co.jp>
2024-02-03 10:42:57 +0900

    fix messsage

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -9,18 +9,14 @@ namespace HelloWorld
     {
         public static void Main()
         {
-            Debug.WriteLine("Hello from nanoFramework!");
+            Debug.WriteLine("HELLO WORLD to debug window");
 
             M5StickC.InitializeScreen();
             nanoFramework.M5Stack.Console.Clear();
             nanoFramework.M5Stack.Console.ForegroundColor = System.Drawing.Color.Green;
-            nanoFramework.M5Stack.Console.WriteLine("HELLO WORLD");
+            nanoFramework.M5Stack.Console.WriteLine("HELLO WORLD to screen");
 
             Thread.Sleep(Timeout.Infinite);
-
-            // Browse our samples repository: https://github.com/nanoframework/samples
-            // Check our documentation online: https://docs.nanoframework.net/
-            // Join our lively Discord community: https://discord.gg/gCyBu8T
         }
     }
 }
